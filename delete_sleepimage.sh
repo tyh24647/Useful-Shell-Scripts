7b03d0c6bbf9f94360fe4a7c7ade179fee576b0a
tyh24647 <tyhostager@yahoo.com>
2017-05-12 22:38:18 -0500

    Update: cleaned up code

--- delete_sleepimage.sh
+++ delete_sleepimage.sh
@@ -11,10 +11,7 @@ GREEN=2
 MAGENTA=5
 
 main() {
-    tput setaf $MAGENTA
-    printf "\nSLEEPIMAGE REMOVER\n"
-    tput sgr0
-
+    output_colored "\nSLEEPIMAGE REMOVER" $MAGENTA
     update_homebrew
     cleanup_homebrew_files
     delete_system_sleep_image
@@ -22,14 +19,12 @@ main() {
 }
 
 delete_system_sleep_image() {
-    echo "Searching for 'sleepimage' file in directory '/private/var/vm/'..."
+    echo "Searching for 'sleepimage' file in '/private/var/vm/'..."
     sleep 2
 
     # check for sleepimage file in default location
     if [ -f /private/var/vm/sleepimage ]; then
-        tput setaf $YELLOW
-        echo "Sleepimage detected"
-        tput sgr0
+        output_colored "Sleepimage detected" $YELLOW
         echo "Deleting 'sleepimage'..."
         echo "Please enter your password..."
         {
@@ -54,18 +49,38 @@ exit_script() {
 }
 
 update_homebrew() {
-    echo "Updating homebrew..."
+    echo "Updating and cleaning homebrew..."
     echo `brew update -v`
+    echo "Done"
+    echo "upgrading homebrew..."
     echo `brew upgrade -v`
-    tput setaf $YELLOW
-    echo "Brew updated successfully"
-    tput sgr0
+    echo "Done"
 }
 
 cleanup_homebrew_files() {
     echo "Cleaning up homebrew files..."
-    tput setaf $YELLOW
-    echo "`brew cleanup -v`Brew successfully cleaned"
+    output_colored "`brew cleanup -v`Brew cleaned successfully" $GREEN
+}
+
+output_colored() {
+    color=-1
+    msg=""
+
+    if [ $# -ge 2 ]; then   # if input is more than 2 params, only use first two
+        msg=$1
+        color=$2
+    elif [ $# -eq 1 ]; then
+        msg=$1
+    elif [ $# -le 0  ]; then
+        return
+    fi
+
+    # set color if applicable
+    if [ $color -ne -1 ] && [ $color -ge 0 ]; then
+        tput setaf $color
+    fi
+
+    echo $msg
     tput sgr0
 }
 
